72137df6acadef5587472f17898a6fac2e4aae43
AvgustZ <zborovski.a@gmail.com>
2020-11-19 17:48:09 +0200

    Task 1 one more Done!

---
+++ Js Task2.js
@@ -0,0 +1,15 @@
+let me = {
+    name: 'Avgust';
+    age: 40;
+    hob: 'jogging';
+    job: {
+        tittle: 'deputy of developement';
+        company:{
+            name: 'griffin';
+            adr: 'Kyiv, Sevastopolska sq';
+        }
+    }
+}
+
+console.log(me);
+console.log(me.job.comany.name)
